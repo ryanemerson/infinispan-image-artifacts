b8499488c770d6f670dc66f0a2bbfac1745e72ad
Ryan Emerson <remerson@redhat.com>
2019-11-26 15:17:16 +0000

    infinispan/infinispan-images#23 Enable CORS configuration

--- config-generator/src/test/groovy/org/infinispan/images/ConfigTest.groovy
+++ config-generator/src/test/groovy/org/infinispan/images/ConfigTest.groovy
@@ -8,6 +8,12 @@ import org.yaml.snakeyaml.Yaml
 
 class ConfigTest {
 
+    private static final String ALLOWED_HEADERS = 'allowed-headers'
+    private static final String ALLOWED_METHODS = 'allowed-methods'
+    private static final String ALLOWED_ORIGINS = 'allowed-origins'
+    private static final String CORS_RULE = 'cors-rule'
+    private static final String CORS_RULES = 'cors-rules'
+    private static final String EXPOSE_HEADERS = 'expose-headers'
     private static final String HOTROD_ENDPOINT = 'hotrod-connector'
     private static final String MEMCACHED_ENDPOINT = 'memcached-connector'
     private static final String REST_ENDPOINT = 'rest-connector'
@@ -111,6 +117,54 @@ class ConfigTest {
             |  clusterName: ${customName}
             """
         assert 'customClusterName' == ispnXml()['cache-container'].transport.@cluster.toString()
+    }
+
+    @Test
+    void testCorsRules() {
+        createConfig """
+            |endpoints:
+            |  rest:
+            |    cors:
+            |      - name: restrict-host1
+            |        allowedOrigins:
+            |          - http://host1
+            |          - https://host1
+            |        allowedMethods:
+            |          - GET
+            |
+            |      - name: allow-all
+            |        allowCredentials: true
+            |        allowedOrigins:
+            |          - '*'
+            |        allowedMethods:
+            |          - GET
+            |          - OPTIONS
+            |          - POST
+            |          - PUT
+            |          - DELETE
+            |        allowedHeaders:
+            |          - X-Custom-Header
+            |          - Upgrade-Insecure-Requests
+            |        exposeHeaders:
+            |          - Key-Content-Type
+            |        maxAgeSeconds: 1
+            """
+        def ispn = ispnXml()
+        assert !ispn.server.endpoints[REST_ENDPOINT].isEmpty()
+        assert 'restrict-host1' == ispn.server.endpoints[REST_ENDPOINT][CORS_RULES][CORS_RULE][0].@name.toString()
+        assert !ispn.server.endpoints[REST_ENDPOINT][CORS_RULES][CORS_RULE][0].@'allow-credentials'.toBoolean()
+        assert 0 == ispn.server.endpoints[REST_ENDPOINT][CORS_RULES][CORS_RULE][0].@'max-age-seconds'.toInteger()
+        assert 'http://host1,https://host1' == ispn.server.endpoints[REST_ENDPOINT][CORS_RULES][CORS_RULE][0][ALLOWED_ORIGINS].toString()
+        assert 'GET' == ispn.server.endpoints[REST_ENDPOINT][CORS_RULES][CORS_RULE][0][ALLOWED_METHODS].toString()
+        assert ispn.server.endpoints[REST_ENDPOINT][CORS_RULES][CORS_RULE][0][ALLOWED_HEADERS].isEmpty()
+        assert ispn.server.endpoints[REST_ENDPOINT][CORS_RULES][CORS_RULE][0][EXPOSE_HEADERS].isEmpty()
+
+        assert 'allow-all' == ispn.server.endpoints[REST_ENDPOINT][CORS_RULES][CORS_RULE][1].@name.toString()
+        assert ispn.server.endpoints[REST_ENDPOINT][CORS_RULES][CORS_RULE][1].@'allow-credentials'.toBoolean()
+        assert 1 == ispn.server.endpoints[REST_ENDPOINT][CORS_RULES][CORS_RULE][1].@'max-age-seconds'.toInteger()
+        assert '*' == ispn.server.endpoints[REST_ENDPOINT][CORS_RULES][CORS_RULE][1][ALLOWED_ORIGINS].toString()
+        assert 'X-Custom-Header,Upgrade-Insecure-Requests' == ispn.server.endpoints[REST_ENDPOINT][CORS_RULES][CORS_RULE][1][ALLOWED_HEADERS].toString()
+        assert 'Key-Content-Type' == ispn.server.endpoints[REST_ENDPOINT][CORS_RULES][CORS_RULE][1][EXPOSE_HEADERS].toString()
     }
 
     private static createConfig(String yaml = "") {
